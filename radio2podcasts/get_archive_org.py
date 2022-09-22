061dbb019c2460fa6743b0534fcb616c263e6a3d
catus felis <catus.f@gmail.com>
2022-09-22 15:24:45 +0000

    Cleanup code

--- radio2podcasts/get_archive_org.py
+++ radio2podcasts/get_archive_org.py
@@ -2,10 +2,8 @@
 """
 
 import collections
-from urllib.parse import urlparse
 import datetime
 import json
-import os
 import pytz
 
 # from podcasts_utils import get_true_url
@@ -68,54 +66,5 @@ def get_articles_from_html(soup, url, no_items, podcast_title, item_titles=None)
                 media=media,
                 type='audio/mpeg')
             )
-
-    # if len(temp_list) < 2: # Just one item, returns it
-    #     return temp_list
-
-    # # Find the first difference if file name
-    # parsed1 = urlparse(temp_list[0].media) # First item
-    # parsed2 = urlparse(temp_list[1].media) # Second item
-
-    # filebase1 = os.path.splitext(os.path.basename(parsed1.path))[0]
-    # filebase2 = os.path.splitext(os.path.basename(parsed2.path))[0]
-
-    # poss = diff_positions(filebase1, filebase2)
-
-    # if len(poss) < 1:
-    #     pos = min(len(filebase1), len(filebase2)) - 1
-    # else:
-    #     pos = poss[0]
-
-    # sorted_list = list()
-
-    # for i in temp_list:
-    #     parsed = urlparse(i.media)
-    #     filebase = os.path.splitext(os.path.basename(parsed.path))[0]
-    #     num = filebase[pos:] # Extract the number part of the filename
-
-    #     if num.isnumeric():
-    #         sorted_list.append((i, num.zfill(2))) # Add one padding zero if this is a number and less than 10
-    #     else:
-    #         sorted_list.append((i, num))
-
-    # sorted_list.sort(key=lambda x: x[1])
-
-    # articles = list()
-
-    # count = 1
-    # for i, num in sorted_list:
-    #     articles.append(
-    #         feed_article(
-    #             link=i.link,
-    #             title=i.title,
-    #             description=i.description,
-    #             pub_date=i.pub_date + datetime.timedelta(minutes=count),
-    #             media=i.media,
-    #             type=i.type)
-    #     )
-    #     count = count + 1
-
-    # for i in articles:
-    #     print(i.pub_date, i.media)
 
     return articles
